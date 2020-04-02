aa8fc2e01f33fe7dcd70cdb61a11d896ab3ce997
Golokhvastov Dmitry <dargon89@mail.ru>
2020-04-02 17:53:27 +0700

    Сделал отрисовку карточек фильмов по циклу из проксов, добавил массив фильмов в index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
+const films = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`];
+
 ReactDOM.render(
     <App
       title = {`The Grand Budapest Hotel`}
       genre = {`Drama`}
       releaseYear = {`2014`}
+      films = {films}
     />,
     document.querySelector(`#root`)
 );
