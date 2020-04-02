a68cfd5bfb5e7f1e1ee7603931b9ca7c09dba4b1
Golokhvastov Dmitry <dargon89@mail.ru>
2020-04-02 16:10:32 +0700

    Исправил замечания линтера

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -44,7 +44,7 @@ const Main = (props) => {
               </p>
 
               <div className="movie-card__buttons">
-               <button className="btn btn--play movie-card__button" type="button">
+                <button className="btn btn--play movie-card__button" type="button">
                   <svg viewBox="0 0 19 19" width="19" height="19">
                     <use xlinkHref="#play-s"></use>
                   </svg>
