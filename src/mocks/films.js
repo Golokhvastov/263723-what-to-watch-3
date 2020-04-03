6f39508e1cda094cf911ce1a6219a85113360228
Golokhvastov Dmitry <dargon89@mail.ru>
2020-04-03 17:36:08 +0700

    Описал моки для маленьких карточек с фильмами для Main

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -1,2 +1,34 @@
 export default [
+  {
+    title: `Aviator`,
+    img: `img/aviator.jpg`,
+  },
+  {
+    title: `We need to talk about Kevin`,
+    img: `img/we-need-to-talk-about-kevin.jpg`,
+  },
+  {
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    img: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+  },
+  {
+    title: `Macbeth`,
+    img: `img/macbeth.jpg`,
+  },
+  {
+    title: `What We Do in the Shadows`,
+    img: `img/what-we-do-in-the-shadows.jpg`,
+  },
+  {
+    title: `Bohemian Rhapsody`,
+    img: `img/bohemian-rhapsody.jpg`,
+  },
+  {
+    title: `Revenant`,
+    img: `img/revenant.jpg`,
+  },
+  {
+    title: `Johnny English`,
+    img: `img/johnny-english.jpg`,
+  }
 ];
