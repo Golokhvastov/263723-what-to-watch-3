d593d8587c7212c5ee5aeae642bc74edf49ab819
Golokhvastov Dmitry <dargon89@mail.ru>
2020-04-03 18:28:16 +0700

    Создал компонент списка карточек movies-list

---
+++ src/components/movies-list/movies-list.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import SmallMovieCard from "../small-movie-card/small-movie-card.jsx";
+
+const MoviesList = (props) => {
+  const {films, onMovieTitleClick} = props;
+
+  return (
+    <div className="catalog__movies-list">
+      {films.map((film, index) => (
+        <SmallMovieCard
+          key={film + index}
+          film = {film}
+          onMovieTitleClick = {onMovieTitleClick}
+        />
+      ))}
+    </div>
+  );
+};
+
+MoviesList.propTypes = {
+  films: PropTypes.arrayOf(
+      PropTypes.shape(
+          title: PropTypes.string.isRequired,
+          img: PropTypes.string,
+      ).isRequired,
+  ),
+  onMovieTitleClick: PropTypes.func.isRequired
+};
+
+export default SmallMovieCard;
